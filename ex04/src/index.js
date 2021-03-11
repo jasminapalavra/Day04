e7d1ddabcdabb0eb62253c6abc955bac9d9deada
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-11 18:37:24 +0100

    Added ex04 src and index.js

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+        // Only change code below this line 
+        var myDecimal = 9.5;
+        var myDecimalE = 65e5;
+        // Only change code above this line
+        return {
+           myDecimal,
+           myDecimalE
+        };
+     }  
+ 
+console.log(main());
+module.exports = main;
