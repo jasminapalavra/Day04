8cf7047c9aa874786aeb6122d5cb022c6a81cc01
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-11 21:30:27 +0100

    Added ex06 src and index.js

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo) {
+   // Only change code below this line 
+    var myQuotient = numOne / numTwo;
+   // Only change code above this line
+   return myQuotient;
+
+}  
+ 
+console.log(parseFloat(main(5.0 , 2.0)));
+console.log(parseFloat(main(16.5 , 5.5)));
+console.log(parseFloat(main(102.0 , 25.5)));
+
+module.exports = main;
