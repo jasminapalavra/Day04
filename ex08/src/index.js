2ddf6127216297f0e4c4004b8e8edc0eac44f26b
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-11 22:38:41 +0100

    Added ex08 src and index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+   // Only change code below this line 
+     var myX = 12 + 3;  
+     var myY = 19 + 17;
+     var myZ = 24 - 20;
+    // Only change code above this line
+   return {
+     myX,
+     myY,
+     myZ
+   }; 
+}
+console.log(main());
+
+module.exports = main;
